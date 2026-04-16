12e8af622acb7e3071bb81d94e7efbb10f6773ac
Ashwin P <techx.paz@gmail.com>
2026-04-16 19:44:19 -0400

    Remove unused useRef import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import { AnimatePresence, motion } from 'motion/react'
 import { useEvents } from './hooks/useEvents'
 import { useNavigation } from './hooks/useNavigation'
